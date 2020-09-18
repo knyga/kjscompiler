bbfe1b9056c5ee9ca80be0e20e6704097517ac29
shikonaj <70107056+shikonaj@users.noreply.github.com>
2020-09-17 20:35:07 -0500

    Add files via upload

---
+++ head.js
@@ -0,0 +1,6 @@
+/**
+ * This is a placeholder file indirectly loaded by eluminate.js
+ * If you would prefer not to load this, update your page as follows:
+ * Add this block: CM_DDX.headScripts = false; after loading eluminate.js but before calling cmSetClientID(...)
+ */
+var __$head={}
